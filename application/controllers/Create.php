e394edb6b5f5527342156cba7098c1abacddc1b4
Addiel <addielJuarez@gmail.com>
2019-05-17 19:13:24 -0500

    conexion a base de datos

--- application/controllers/Create.php
+++ application/controllers/Create.php
@@ -1,6 +1,27 @@
 <?php
 class Create extends CI_Controller{
     public function index(){
-        echo "Create";
+        $this->load->view('Create_view');
+
+        $username= "jesus95santiago@gmail.com";
+        $password="123456";
+        $name="Jesus Santiago";
+        $phone="8115899028";
+        $address="Captain Lucas G 816, al lado de bodeguita";
+        $matricula="10457";
+        $databusqueda = array ("email" => $username);
+        $busquedacorreo = $this -> db-> get_where ("alumnos",$databusqueda);
+        
+
+        //$this->load->database();
+        $data = array('email' =>$username,
+        'password'=>$password,
+        'name'=>$name,
+        'phone'=>$phone,
+        'address'=>$address,
+        'matricula'=>$matricula);
+        //echo json_encode($data);
+        $this->db->insert('alumnos',$data);
+        
     }
 }
