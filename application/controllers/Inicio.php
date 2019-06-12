6927d29542c23e6672a90f533a39e9e0319b09ac
Addiel <addielJuarez@gmail.com>
2019-06-11 19:58:05 -0500

    creacion de modelo

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -5,5 +5,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Inicio extends CI_Controller {
     public function index(){
         $this->load->view('Inicio');
+
+        // $this->load->helper('file');
+
+
+
+
+
+
     }
 }
---
+++ application/models/usuarios.php
@@ -0,0 +1,31 @@
+
+<?php
+
+class Usuarios extends CI_Model{
+
+    public function __construct(){
+        $this->load->database();
+    }
+
+    public function acceso(){
+
+    }
+
+    public function registro(){
+
+    }
+
+    public function busquedaUsuario(){
+
+    }
+
+    public function todosUsuarios(){
+
+    }
+
+}
+
+
+
+
+
